d633b150af73a3055d079239f600693127a67b52
KienDev <ngotrungkien2k2@gmail.com>
2022-12-28 21:49:32 +0700

    update-data

--- src/components/firebase/FirebaseApp.js
+++ src/components/firebase/FirebaseApp.js
@@ -1,3 +1,4 @@
+import { async } from "@firebase/util";
 import {
     collection,
     getDocs,
@@ -6,6 +7,7 @@ import {
     doc,
     onSnapshot,
     serverTimestamp,
+    updateDoc,
 } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 import { db } from "./firebase-config";
@@ -73,6 +75,16 @@ const FirebaseApp = () => {
         await deleteDoc(docRefDelete);
         console.log("succes");
     };
+
+    const handleUpdate = async (e) => {
+        e.preventDefault();
+        const docRefUpdate = doc(db, "posts", postId);
+        // có thêm data để update
+        await updateDoc(docRefUpdate, {
+            title: "this is a new title from update function",
+        });
+        console.log("succes");
+    };
     return (
         <div>
             <div className="w-full max-w-[500px] mx-auto bg-white shadow-lg p-5">
@@ -133,6 +145,27 @@ const FirebaseApp = () => {
                                 </div>
                             </div>
                         ))}
+                </div>
+                <div className="mb-10">
+                    <h1 className="font-semibold text-center">
+                        handle update data follow id
+                    </h1>
+                    <form onSubmit={handleUpdate}>
+                        <input
+                            name="postId"
+                            type="text"
+                            className="w-full p-3 mb-5 border border-gray-300 rounded outline-none focus:border-blue-500"
+                            onChange={(e) => setPostId(e.target.value)}
+                            placeholder="Enter your id"
+                        />
+
+                        <button
+                            type="submit"
+                            className="w-full p-3 text-sm font-medium text-white bg-red-500 rounded-lg"
+                        >
+                            update
+                        </button>
+                    </form>
                 </div>
             </div>
         </div>
